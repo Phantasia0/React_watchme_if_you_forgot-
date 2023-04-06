9f3847a812847aeac6453baaa5df9323301927a6
Phantasia0 <ncprog1@gmail.com>
2023-04-07 03:07:17 +0900

    ch6.컴포넌트 반복에 관하여

---
+++ ch6/src/App.js
@@ -0,0 +1,11 @@
+import IterationSample from './components/IterationSample';
+
+function App() {
+  return (
+    <div className="App">
+      <IterationSample />
+    </div>
+  );
+}
+
+export default App;
