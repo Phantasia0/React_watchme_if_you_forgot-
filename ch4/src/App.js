c324ce86cbd52f3f638163e430061793155e02d1
Phantasia0 <ncprog1@gmail.com>
2023-04-06 17:11:56 +0900

    ch4.이벤트에 관하여

---
+++ ch4/src/App.js
@@ -0,0 +1,13 @@
+// import EventPractice from './components/EventPractice';
+import EventPractice2 from './components/EventPractice2';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <EventPractice /> */}
+      <EventPractice2 />
+    </div>
+  );
+}
+
+export default App;
