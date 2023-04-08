664fc7f67bce91b63782ac1121da74e164a40ddc
Phantasia0 <ncprog1@gmail.com>
2023-04-09 04:47:59 +0900

    ch14.newsApi 뷰어에 관하여.useState

---
+++ ch14/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useCallback } from 'react';
+// import axios from 'axios';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       'https://newsapi.org/v2/top-headlines?country=kr&apiKey=5767607f4303429cbd061e76ee7936b8',
+  //     );
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <button onClick={onClick}>불러오기</button>
+  //     </div>
+  //     {data && (
+  //       <textarea
+  //         rows={7}
+  //         value={JSON.stringify(data, null, 2)}
+  //         readOnly={true}
+  //       />
+  //     )}
+  //   </div>
+  // );
+
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default App;
---
+++ ch14/src/components/NewsList.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=5767607f4303429cbd061e76ee7936b8`,
+        );
+        setArticles(response.data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [category]);
+
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+// const sampleArticle = {
+//   title: '제목',
+//   description: '내용',
+//   url: 'https://google.com',
+//   urlToImage: 'https://via.placeholder.com/160',
+// };
