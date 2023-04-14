3b50a13b937efc2f7b5f026fb92bcc1b7ec4164a
Phantasia0 <ncprog1@gmail.com>
2023-04-14 10:13:40 +0900

    ch25.Post 글쓰기 기능 구현에 관하여

---
+++ ch25/blog/blog-frontend/src/containers/write/EditorContainer.js
@@ -0,0 +1,31 @@
+import React, { useCallback, useEffect } from 'react';
+import Editor from '../../components/write/Editor';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeField, initialize } from '../../modules/write';
+
+const EditorContainer = () => {
+  const dispatch = useDispatch();
+  const { title, body } = useSelector(({ write }) => ({
+    title: write.title,
+    body: write.body,
+  }));
+
+  const onChangeField = useCallback(
+    (payload) => dispatch(changeField(payload)),
+    [dispatch],
+  );
+
+  useEffect(() => {
+    return () => {
+      dispatch(initialize());
+    };
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Editor title={title} body={body} onChangeField={onChangeField} />
+    </div>
+  );
+};
+
+export default EditorContainer;
