06f14210f7d131dd84ce997ca46c577f26fa67c3
Phantasia0 <ncprog1@gmail.com>
2023-04-14 15:16:42 +0900

    ch26.Post 조회와 페이지네이션에 관하여

---
+++ ch26/blog/blog-frontend/src/components/common/SubInfo.js
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+import palette from '../../lib/styles/palette';
+import { Link } from 'react-router-dom';
+
+const SubInfoBlock = styled.div`
+  ${(props) =>
+    props.hasMarginTop &&
+    css`
+      margin-top: 1rem;
+    `}
+  color: ${palette.gray[6]};
+  span + span:before {
+    content: '\\B7';
+    color: ${palette.gray[4]};
+    padding-left: 0.25rem;
+    padding-right: 0.25rem;
+  }
+`;
+
+const SubInfo = ({ username, publishedDate, hasMarginTop }) => {
+  return (
+    <SubInfoBlock hasMarginTop={hasMarginTop}>
+      <span>
+        <b>
+          <Link to={`/user/${username}`}>{username}</Link>
+        </b>
+      </span>
+      <span>{new Date(publishedDate).toLocaleDateString()}</span>
+    </SubInfoBlock>
+  );
+};
+
+export default SubInfo;
---
+++ ch26/blog/blog-frontend/src/lib/api/client.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const client = axios.create();
+
+/*
+  AXIOS 글로벌 설정용 공통 인스턴스
+ */
+
+export default client;
