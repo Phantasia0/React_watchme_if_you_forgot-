adaa5cd799a2f195d4be82c0451d4ae84a7d7470
Phantasia0 <ncprog1@gmail.com>
2023-04-15 10:02:27 +0900

    ch27.Post 수정, 삭제에 관하여

---
+++ ch27/blog/blog-backend/src/api/posts/index.js
@@ -0,0 +1,21 @@
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl.js';
+import checkLoggedIn from '../../lib/checkLoggedIn.js';
+
+const posts = new Router();
+
+posts.get('/', postsCtrl.list);
+posts.post('/', checkLoggedIn, postsCtrl.write);
+
+const post = new Router();
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+
+posts.use('/:id', postsCtrl.getPostById, post.routes());
+
+// posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+// posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+// posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+
+export default posts;
---
+++ ch27/blog/blog-frontend/src/containers/post/PostViewerContainer.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { readPost, unloadPost } from '../../modules/post';
+import PostViewer from '../../components/post/PostViewer';
+import PostActionButtons from '../../components/post/PostActionButtons';
+import { removePost } from '../../lib/api/posts';
+import { setOriginalPost } from '../../modules/write';
+
+const PostViewerContainer = () => {
+  const { postId } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { post, error, loading, user } = useSelector(
+    ({ post, loading, user }) => ({
+      post: post.post,
+      error: post.error,
+      loading: loading['post/READ_POST'],
+      user: user.user,
+    }),
+  );
+
+  useEffect(() => {
+    dispatch(readPost(postId));
+    return () => {
+      dispatch(unloadPost());
+    };
+  }, [dispatch, postId]);
+
+  const onRemove = async () => {
+    try {
+      await removePost(postId);
+      navigate('/');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    navigate('/write');
+  };
+
+  const ownPost = (user && user._id) === (post && post.user._id);
+
+  return (
+    <PostViewer
+      post={post}
+      error={error}
+      loading={loading}
+      actionButtons={
+        ownPost && <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
+      }
+    />
+  );
+};
+
+export default PostViewerContainer;
